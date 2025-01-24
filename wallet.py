0f1e487ddeaf7611fab18bc0838a6f002da462f4
SPO <stephane.pointu@siveo.net>
2025-01-24 07:09:25 +0100

    Ajout de documentation sommaire

--- wallet.py
+++ wallet.py
@@ -1,12 +1,16 @@
 class Wallet:
     def __init__(self, balance):
+        # Initialisation de la classe
         self.balance = balancer
 
     def set_balance(self, val):
+        # Ajout d'une somme à la balance
         self.balance = self.balance + val
 
     def get_balance(self):
+        # Récupération de la balance
         return self.balance
 
     def remove_balance(self, val):
+        # Soustraction d'une somme de la balance
         self.balance = self.balance - val
